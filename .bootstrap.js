da3596e93ee07276f54b8958093266c4657bbf39
Vibesdk <vibesdk-bot@cloudflare.com>
2026-04-10 22:36:21 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add react-helmet-async@^2.0.5"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
